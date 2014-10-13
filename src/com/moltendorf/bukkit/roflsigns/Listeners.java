86aea121dd1227cf15f38d3f9acaaca78672800d
Matthew Oltendorf <moltendorf@gmail.com>
2014-10-12 21:53:27 -0700

    Initial version.

---
+++ src/com/moltendorf/bukkit/roflsigns/Listeners.java
@@ -0,0 +1,46 @@
+package com.moltendorf.bukkit.roflsigns;
+
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.block.SignChangeEvent;
+
+/**
+ * Listener register.
+ *
+ * @author moltendorf
+ */
+public class Listeners implements Listener {
+
+	final protected Plugin plugin;
+
+	protected Listeners(final Plugin instance) {
+		plugin = instance;
+	}
+
+	@EventHandler(priority = EventPriority.LOWEST)
+	public void BlockPlaceEventLowestHandler(final BlockPlaceEvent event) {
+		if (!event.getPlayer().getName().equals("roflcopterss99")) {
+			return;
+		}
+
+		if (event.getBlockPlaced().getType() == Material.SIGN_POST) {
+			event.setCancelled(true);
+		}
+	}
+
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void BlockPlaceEventHighestHandler(final BlockPlaceEvent event) {
+		if (!event.getPlayer().getName().equals("roflcopterss99")) {
+			return;
+		}
+
+		if (event.isCancelled()) {
+			if (event.getBlockPlaced().getType() == Material.SIGN_POST) {
+				event.setCancelled(false);
+			}
+		}
+	}
+}
